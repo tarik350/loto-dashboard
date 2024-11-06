8fcef09ab5f6ea5a0b6557dbe666ade0b33e239a
tarik350 <80385670+tarik350@users.noreply.github.com>
2024-11-06 17:51:49 +0300

    additional dashboard cleanup

--- src/SessionProvider.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { ReactNode } from "react";
-import { SessionProvider as Provider } from "next-auth/react";
-export default function SessionProvider({ children }: { children: ReactNode }) {
-  return <Provider>{children}</Provider>;
-}
